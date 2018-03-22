b6f4dad417326fc5924fac59df5064877369be52
style198708 <style1987@sina.cn>
2018-03-22 13:57:01 +0800

    添加项目文件。

---
+++ CoreWebApiGetway/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace CoreWebApiGetway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           // IWebHostBuilder builder = new WebHostBuilder();
+           // builder.ConfigureServices(s =>
+           // {
+           //     s.AddSingleton(builder);
+           // });
+           // builder.UseKestrel()
+           //.UseUrls("http://localhost:5000")
+           //.UseContentRoot(Directory.GetCurrentDirectory())
+           //.UseIISIntegration()
+           //.UseStartup<Startup>()
+           //.UseApplicationInsights();
+           // var host = builder.Build();
+           // host.Run();
+
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            IWebHostBuilder builder = new WebHostBuilder();
+            return builder.ConfigureServices(s =>
+                    {
+                         s.AddSingleton(builder);
+                    })
+                  .UseKestrel()
+                  .UseUrls("http://localhost:5000")
+                  .UseContentRoot(Directory.GetCurrentDirectory())
+                  .UseIISIntegration()
+                  .UseStartup<Startup>()
+                  .UseApplicationInsights()
+                  .Build();
+        }
+    }
+}
---
+++ CoreWebApiGetway/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CacheManager.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ocelot.Configuration;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace CoreWebApiGetway
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment environment)
+        {
+            var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
+                   builder.SetBasePath(environment.ContentRootPath)
+                   .AddJsonFile("appsettings.json", false, reloadOnChange: true)
+                   .AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
+                   .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Action<ConfigurationBuilderCachePart> settings = (x) =>
+            {
+                x.WithMicrosoftLogging(log =>
+                {
+                    log.AddConsole(LogLevel.Debug);
+
+                }).WithDictionaryHandle();
+            };
+            services.AddOcelot(Configuration, settings);
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseOcelot();
+        }
+    }
+}
